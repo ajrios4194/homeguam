387fd5facdbdf5557acf5e446d94403a44770e94
Aaron Rios <aaronjohnrios@gmail.com>
2019-01-21 06:44:57 +0000

    added new fields for application

--- models/applicant.js
+++ models/applicant.js
@@ -11,8 +11,13 @@ var appSchema = new mongoose.Schema({
     app_type: Number,
     bday: Date,
     school_type: String,
+    year: String,
+    specialty: String,
     gpa: String,
-    essay: String
+    essay: String,
+    extra: String,
+    discover: String,
+    major: String
 });
 
 appSchema.plugin(passportLocalMongoose, {usernameField: 'email'});
